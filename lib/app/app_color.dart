e6b8b74edc31e26cb26f301d0857da6844c1f476
ravshan2101 <84902019+ravshan2101@users.noreply.github.com>
2023-04-10 17:05:49 +0500

    color added

--- lib/app/app_color.dart
+++ lib/app/app_color.dart
@@ -5,4 +5,5 @@ abstract class AppColor {
   static const Color contColor = Color(0xefefD9DFE1);
   static const Color whiteColor = Colors.white;
   static const Color backgrColor = Color.fromARGB(255, 250, 246, 246);
+  static const Color black = Colors.black;
 }
